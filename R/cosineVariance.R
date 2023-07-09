1968dcb6dee155360bacd5e3d0762191e2fbeea7
Ian Smith <iamsinht@gmail.com>
2023-07-09 06:49:52 -0400

    Migrated functionality to package.

---
+++ R/cosineVariance.R
@@ -0,0 +1,62 @@
+library(dplyr)
+library(gridExtra)
+library(perturbKit)
+library(pracma)
+library(MASS)
+
+
+# This function is used to sample a multivariate normal distribution
+# with eigenvalues drawn from some distribution and compute var(cos).
+# This can then be used to test the variance conjecture. 
+# Add parameters to allow specification of the function from which
+# eigenvalues are sampled.
+
+#' Sample the variance of cosine similarity for vectors drawn from a multivarite normal
+#' distribution with eigenvalues drawn from a Gamma distribution. 
+#' 
+#' @param ndim numeric, a vector of dimensions to sample. Default seq(100, 1000, 100)
+#' @param iter Number of iterations to use for a particular dimension and eigenvalue distribution
+#' @param distiter Number of eigenvalue distributions to run for each dimension
+#' @param sPar Numeric, vector of gamma distribution hyperparameters for the eigenvalues. The eigenvalues
+#' are sampled from a gamma distribution with shape parameter gamma(shape=sPar[1], rate=sPar[2]). and rate
+#' parameter gamma(shape=sPar[3], rate=sPar[4])
+#' 
+#' @returns Dataframe with columns: dimension, theoretical variance based on eigenvalues, theoretical variance 
+#' on observed eigenvalues, and empirical variance. 
+#' @export
+#' @importFrom stats rgamma
+sampleVarCos <- function(ndim=seq(100, 1000, 100), iter=3, distiter=10, sPar=c(1, 2, 1, 2)){
+  
+  mylen <- length(ndim)*iter*distiter
+  resdf <- data.frame(ndim=numeric(mylen), theoryVar=numeric(mylen), theoryEmpVar=numeric(mylen), obsVar=numeric(mylen))
+  k <- 1
+  
+  for (mydim in ndim){
+    for (ii in seq_len(distiter)){
+      print(sprintf("ii = %d", ii))
+      a <- stats::rgamma(1, shape=sPar[1], rate=sPar[2])
+      b <- stats::rgamma(1, shape=sPar[3], rate=sPar[4])
+      
+      for (jj in seq_len(iter)){
+        sigmat <- diag(rgamma(mydim, shape=a, rate=b))
+        lambda <- diag(sigmat)
+        
+        x <- t(MASS::mvrnorm(n=2000, mu=numeric(mydim), Sigma=sigmat))
+        xpr <- stats::prcomp(t(x))
+        
+        xcos <- perturbKit::cosine(x, x)
+        
+        ovar <- stats::var(xcos[upper.tri(xcos)])
+        tvar <- sum(lambda ** 2)/(sum(lambda) ** 2)
+        
+        tempvar <- sum(xpr$sdev^2 ** 2)/(sum(xpr$sdev^2))^2
+        
+        resdf[k,] <- c(mydim, tvar, tempvar, ovar)      
+        k <- k + 1
+      }
+    }
+  }
+  
+  return (resdf)
+  
+}
---
+++ R/metasignatures.R
@@ -0,0 +1,212 @@
+#' Extract metasignatures
+#' getMetasigs takes an input of a matrix of signatures, where the rows are features and the
+#' columns are signatures.  It generates metasignatures by averaging k signatures together, where
+#' k is the given parameter (default 1). 
+#' 
+#' @param mymat Numeric matrix of dimension features x samples
+#' @param k Numeric, number of signatures to sample and average without replacement
+#' @param return2 logical: if true, it returns a list of two disjoint metasignatures of size k. 
+#' @param returnk logical: if true and return2 is false, returns as many disjoint 
+#' metasignatures of size k as possible. If return2 and returnk are both false, returns one metasignature
+#' 
+#' @returns some number of metasignatures depending on parameters specified
+#' @export
+#' @importFrom cmapR GCT
+getMetasigs <- function(mymat, k=1, return2=1, returnk=0){
+
+  if (k==1){
+    if (return2 == 0){
+      return(mymat[, sample(dim(mymat)[2], 1)])
+    } else {
+      ix <- sample(dim(mymat)[2], 2)
+      return(cbind(mymat[, ix[1]], mymat[, ix[2]]))
+    }
+  }
+  
+  if (return2 == 0){
+    ix <- sample(dim(mymat)[2], k)
+    return(rowMeans(mymat[, ix]))
+  } else if (returnk==1) {
+    
+    ix <- sample(dim(mymat)[2], floor(dim(mymat)[2]/k) * k)
+    return(sapply(seq(floor(dim(mymat)[2]/k)), FUN=function(x) rowMeans(mymat[, ix[(k*(x-1)+1):(k*x)]])))
+
+  } else {
+    if (2*k <= dim(mymat)[2]){
+      ix <- sample(dim(mymat)[2], 2*k)
+      return(sapply(seq(2), FUN=function(x) rowMeans(mymat[, ix[(k*(x-1)+1):(k*x)]])))
+      #return(list(rowMeans(mymat[, ix[1:n]]), rowMeans(mymat[, ix[(n+1):(2*n)]])))
+    } else {
+      return(c(0,0))
+    }
+  }
+  
+}
+
+
+#' getMetaCorr
+#' For an input matrix, compute the Pearson's correlation of metasignatures drawn from that matrix
+#' as a function of the metasignature size k. 
+#' @param mymat Numeric matrix of dimension features x samples
+#' @param kmax Numeric, maximum value to use for metasignature size k. 
+#' @param iter Numeric, the number of pairs of disjoint metasignatures to sample for each value of 
+#' metasignature size k. Default = 100. 
+#' @param useall Logical, whether to sample a range of values for kmax or use all values. Default FALSE, 
+#' deprecated. 
+#' 
+#' @returns A list of (1) cormat - a matrix with dimension kmax x iter, where each entry is the Pearson's
+#' correlation of a metasignature of size k, 
+#' (2) cordf - a data frame with columns metasize, mean correlation, sd correlation summarizing the 
+#' correlation of metasignatures generated from the input matrix. 
+#' @export
+getMetaCorr <- function(mymat, kmax, iter=100, useall=FALSE){
+  kmax <- min(kmax, floor(dim(mymat)[2]/2))
+  
+  # Revisit:
+  # if (kmax > 50){
+  #   seqvals <- c(seq(10), seq(15, kmax, 5))
+  #   seqvals <- seqvals[seqvals <= kmax]
+  # } else {
+  #   seqvals <- seq(kmax)
+  # }
+  
+  # if (!useall){
+  #   seqvals <- c(seq(10), seq(15, kmax, 5))
+  #   seqvals <- seqvals[seqvals <= kmax]
+  # } else {
+  #   seqvals <- seq(1, kmax)
+  # }
+  seqvals <- seq(1, kmax, 1)
+  
+  cormat <- matrix(numeric(iter*length(seqvals)), nrow=length(seqvals), dimnames=list(seqvals))
+  
+  for (jj in seq(length(seqvals))){
+    ii <- seqvals[jj]
+    a <- lapply(seq(iter), FUN=function(x) getMetasigs(mymat, k=ii, return2=1))
+    cormat[jj,] <- sapply(a, FUN=function(x) stats::cor(x)[2])
+    #cormat[ii,] <- sapply(seq(iter), FUN=function(x) cor(a[[1,x]], a[[2,x]]))
+  }
+  
+  cordf <- data.frame(metasize=seqvals, meancor=rowMeans(cormat), sdcor=apply(cormat, 1, stats::sd))
+  return(list(cormat=cormat, cordf=cordf))
+}
+
+
+#' bootstrapMaxMetaCorr
+#' For an input matrix, bootstraps the correlation of maximal disjoint metasignatures, i.e.
+#' those of size floor(dim(mymat)[2]/2). 
+#' @param mymat Numeric matrix of dimension features x samples
+#' @param iter Numeric, the number of pairs of disjoint metasignatures to sample
+#' 
+#' @returns Numeric vector of length iter corresponding to the Pearson correlations of maximal disjoint
+#' metasignatures sampled from the matrix. 
+#' @export
+bootstrapMaxMetaCorr <- function(mymat, iter=100){
+  kmax <- floor(dim(mymat)[2]/2)
+  
+  mycors <- sapply(seq(iter), FUN=function(x) stats::cor(getMetasigs(mymat, k=kmax, return2=1))[2])
+  return(mycors)
+}
+
+
+
+# This function is inordinately slow (because of wtcs)
+#' getMetaSim
+#' For an input matrix, compute the similarity of metasignatures drawn from that matrix
+#' as a function of the metasignature size k. Analogous to getMetaCorr, but allows Pearson, 
+#' Spearman, cosine similarity, and L1000's weighted connectivity score (wtcs).
+#' @param mymat Numeric matrix of dimension features x samples
+#' @param kmax Numeric, maximum value to use for metasignature size k. 
+#' @param iter Numeric, number of pairs of disjoint metasignatures to sample for each value of 
+#' metasignature size k. Default = 100. 
+#' @param metric One of "pearson", "spearman", "wtcs", or "cosine". 
+#' 
+#' @returns List of: simmat - similarity matrix of dimension kmax x iter with the similarities 
+#' of the metasignatures; cordf - data frame summarizing the mean and standard deviation of the
+#' similarities; metric - string indicating which metric was used. 
+#' @export
+#' @importFrom coop cosine
+getMetaSim <- function(mymat, kmax, iter=100, metric="pearson"){
+  
+  metric <- match.arg(metric, c("pearson", "spearman", "wtcs", "cosine"))
+  kmax <- min(kmax, floor(dim(mymat)[2]/2))
+  
+  seqvals <- seq(kmax)
+  
+  # if (kmax > 50){
+  #   seqvals <- seq(5, kmax, 5)
+  # } else {
+  #   seqvals <- seq(kmax)
+  # }
+  
+  simmat <- matrix(numeric(length(seqvals)*iter), nrow=length(seqvals))
+  
+  myfunc <- switch(metric, 
+                   pearson = function(x) stats::cor(x)[2], 
+                   spearman = function(x) stats::cor(x, method="spearman")[2], 
+                   wtcs = function(x) perturbKit::calcSimBlock(cmapR::GCT(mat=x, cid=as.character(seq(dim(x)[2]))), 
+                                                               cmapR::GCT(mat=x, cid=as.character(seq(dim(x)[2]))), metric=metric)[2], 
+                   cosine = function(x) coop::cosine(x)[2]) 
+                   #wtcsmat = function(x) compute_cs_mats(x, x)[2])  # <- wtf is this
+  
+  for (ii in seq_along(seqvals)){
+    a <- lapply(seq(iter), FUN=function(x) getMetasigs(mymat, k=seqvals[ii], return2=1))
+    simmat[ii,] <- sapply(a, FUN=myfunc)
+  }
+  
+  cordf <- data.frame(metasize=seqvals, meanSim=rowMeans(simmat), sdSim=apply(simmat, 1, stats::sd))
+  rownames(simmat) <- seqvals
+  return(list(simmat=simmat, cordf=cordf, metric=metric))
+}
+
+
+#' getMetaSimDs
+#' Apply getMetaSim to each group within a dataset. 
+#' @param mymat Numeric matrix of dimension features x samples
+#' @param groupings Vector of class labels, either character or numeric. For each class, metacorrelation 
+#' is computed.
+#' @param kmax Numeric, maximum value to use for metasignature size k (default 1000). For any group with 
+#' fewer than 2*kmax signatures, computes up to floor(groupsize/2). 
+#' @param iter Numeric, number of pairs of disjoint metasignatures to sample for each value of metasize k. 
+#' Default  = 100.
+#' @param metric One of "pearson" (default), "spearman", "wtcs", or "cosine". 
+getMetaSimDs <- function(mymat, groupings, kmax=1000, iter=100, metric="pearson"){
+  
+  if (dim(mymat)[2] != length(groupings)){
+    return("Error: length of groupings does not match number of columns of input matrix.")
+  }
+  
+  ret <- list()
+  mygrps <- names(table(groupings)[table(groupings) >= 6])
+  
+  for (ii in seq_along(mygrps)){
+    if (ii %% 50 == 0){
+      print(sprintf("%d/%d", ii, length(mygrps)))
+    }
+    agrp <- mygrps[ii]
+    ix <- which(groupings == agrp)
+    
+    metaStr <- getMetaSim(mymat[, ix], kmax=kmax, iter=iter, metric=metric)
+    ret <- c(ret, list(metaStr))
+  }
+  
+  names(ret) <- mygrps
+  
+  return(ret)
+}
+
+
+#' ecdfPointwise
+#' Takes a numeric vector and computes an ecdf on user-input values of x. This is particularly 
+#' useful for overlaying multiple ECDFs at the same points. 
+#' @param mydat numeric vector to compute an ECDF
+#' @param breaks vector of points at which to compute the ECDF.
+#' 
+#' @returns A dataframe with columns x - the domain, the breaks; and y - the fraction of input 
+#' values less than x. 
+#' @export
+ecdfPointwise <- function(mydat, breaks){
+  h <- graphics::hist(mydat, breaks=breaks, plot=FALSE)
+  y0 <- mean(mydat < breaks[1])
+  return(data.frame(x=breaks, y=c(y0, cumsum(h$counts)/length(mydat))))
+}
